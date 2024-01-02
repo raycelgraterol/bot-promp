303e72b0dfbaa7f1b5101b2a36c5686e7f781fc2
Raycel Graterol <craycel.0@gmail.com>
2024-01-02 14:13:03 -0400

    comment notes proccess

--- flows/custom-flows/flowNotes.js
+++ flows/custom-flows/flowNotes.js
@@ -5,18 +5,18 @@ const flowNotaDeVoz = addKeyword(EVENTS.VOICE_NOTE)
     .addAction(async (ctx, ctxFn) => {
         
         console.log(`[Flow Smart VoiceNote]`)
-        const employeesAddon = ctxFn.extensions.employeesAddon
+        //const employeesAddon = ctxFn.extensions.employeesAddon
         await ctxFn.flowDynamic("dame un momento para escucharte...🙉");
-        const text = await handlerAI(ctx);
+        // const text = await handlerAI(ctx);
 
-        const currentState = ctxFn.state.getMyState();
-        console.log(text)
+        // const currentState = ctxFn.state.getMyState();
+        // console.log(text)
 
-        // const fullSentence = `${currentState?.answer ?? ""}. ${text}`;
-        // const { employee, answer } = await employeesAddon.determine(fullSentence);
-        // ctxFn.state.update({ answer });
-        // if (employee) employeesAddon.gotoFlow(employee, ctxFn);
-        // if (!employee) ctxFn.gotoFlow(notEmployee);
+        // // const fullSentence = `${currentState?.answer ?? ""}. ${text}`;
+        // // const { employee, answer } = await employeesAddon.determine(fullSentence);
+        // // ctxFn.state.update({ answer });
+        // // if (employee) employeesAddon.gotoFlow(employee, ctxFn);
+        // // if (!employee) ctxFn.gotoFlow(notEmployee);
     });
 
 module.exports = flowNotaDeVoz;
