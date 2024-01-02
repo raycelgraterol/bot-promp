f75e115e2ccc43bcd5894c6d75815205e1be778e
Raycel Graterol <craycel.0@gmail.com>
2024-01-02 15:02:56 -0400

    changes in page and testing

--- flows/custom-flows/flowNotes.js
+++ flows/custom-flows/flowNotes.js
@@ -6,17 +6,17 @@ const flowNotaDeVoz = addKeyword(EVENTS.VOICE_NOTE)
         
         console.log(`[Flow Smart VoiceNote]`)
         //const employeesAddon = ctxFn.extensions.employeesAddon
+        
         await ctxFn.flowDynamic("dame un momento para escucharte...🙉");
-        // const text = await handlerAI(ctx);
+        const text = await handlerAI(ctx);
+        console.log(text)
 
-        // const currentState = ctxFn.state.getMyState();
-        // console.log(text)
-
-        // // const fullSentence = `${currentState?.answer ?? ""}. ${text}`;
-        // // const { employee, answer } = await employeesAddon.determine(fullSentence);
-        // // ctxFn.state.update({ answer });
-        // // if (employee) employeesAddon.gotoFlow(employee, ctxFn);
-        // // if (!employee) ctxFn.gotoFlow(notEmployee);
+        //const currentState = ctxFn.state.getMyState();
+        // const fullSentence = `${currentState?.answer ?? ""}. ${text}`;
+        // const { employee, answer } = await employeesAddon.determine(fullSentence);
+        // ctxFn.state.update({ answer });
+        // if (employee) employeesAddon.gotoFlow(employee, ctxFn);
+        // if (!employee) ctxFn.gotoFlow(notEmployee);
     });
 
 module.exports = flowNotaDeVoz;
