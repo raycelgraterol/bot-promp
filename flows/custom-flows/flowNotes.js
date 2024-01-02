1a2386d1953638228794653cfe65ba35403642b3
Raycel Graterol <craycel.0@gmail.com>
2024-01-02 13:24:49 -0400

    new package

--- flows/custom-flows/flowNotes.js
+++ flows/custom-flows/flowNotes.js
@@ -1,6 +1,22 @@
 const { addKeyword, EVENTS } = require("@bot-whatsapp/bot");
+const { handlerAI } = require("../../utils/utils");
 
 const flowNotaDeVoz = addKeyword(EVENTS.VOICE_NOTE)
-    .addAnswer('Dame un momento para escuchar la nota de voz')
+    .addAction(async (ctx, ctxFn) => {
+        
+        console.log(`[Flow Smart VoiceNote]`)
+        const employeesAddon = ctxFn.extensions.employeesAddon
+        await ctxFn.flowDynamic("dame un momento para escucharte...🙉");
+        const text = await handlerAI(ctx);
+
+        const currentState = ctxFn.state.getMyState();
+        console.log(text)
+
+        // const fullSentence = `${currentState?.answer ?? ""}. ${text}`;
+        // const { employee, answer } = await employeesAddon.determine(fullSentence);
+        // ctxFn.state.update({ answer });
+        // if (employee) employeesAddon.gotoFlow(employee, ctxFn);
+        // if (!employee) ctxFn.gotoFlow(notEmployee);
+    });
 
 module.exports = flowNotaDeVoz;
